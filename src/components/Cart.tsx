c53d02e0dbb02418995df02c8b53a18f628d4aa1
arod571431 <nick930212@gmail.com>
2024-12-14 01:38:43 +0800

    Fix Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import {Table,Flex,Layout} from 'antd';
 import { Divider, Radio } from 'antd';
 import type { TableColumnsType, TableProps } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+import {Button, Nav} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import UserBadge from "./UserBadge.tsx";
+const { Content } = Layout;
 
+/* type TableRowSelection<T extends object = object> = TableProps<T>['rowSelection']; */
 /*handle API object*/
 interface ProductInCart {
-    key: React.Key;
+    key: React.key;
     ID: number;
     Name: string;
     Images: string[];
@@ -30,6 +34,7 @@ const layoutStyle = {
     width: 'calc(80% - 8px)',
     maxWidth: 'calc(80% - 8px)',
 };
+/*Handle Shopping Cart Table*/
 const CartTable:ColumnsType<ProductInCart> = [
     {
         title: "圖片",
@@ -37,32 +42,38 @@ const CartTable:ColumnsType<ProductInCart> = [
         render: (t, r) => <img src={`${r.Images}`} style={{ width: '50px' , height: '50px' }} />
     },
     {
-        title: '商品名稱',
-        dataIndex: 'Name',
+        title: "商品名稱",
+        align: "center",
+        dataIndex: "Name",
+        key: "name"
     },
     {
-        title: '單價',
-        dataIndex: 'Price',
+        title: "單價",
+        align: "center",
+        dataIndex: "Price",
+        key: "price"
     },
     {
-        title: '數量',
-        dataIndex: 'Quantity',
+        title: "數量",
+        align: "center",
+        dataIndex: "Quantity",
+        key: "amount"
     },
-];
-const rowSelection: TableProps<ProductInCart>['rowSelection'] = {
-    onChange: (selectedRowKeys: React.Key[], selectedRows: ProductInCart[]) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    {
+        title: "總計",
+        align: "center",
+        dataIndex: 'totalPrice',
+        key: "totalPrice",
+        render: (text, record, _) => {
+            return record["Quantity"] * record["Price"];
+        }
     }
-};
+];
 const Cart: React.FC = () => {
+    /* Get Cart Info */
     const [Product, setcatchProduct] = useState<ProductInCart>([]);
     const [hasProduct,sethasProduct] = useState(false);
     const [error, seterror] = useState(false);
-
-
-    const [productselect,setproductselect] = useState<'checkbox' | 'radio'>('checkbox');
-
-
     const token = localStorage.getItem('token');
     const fetchUserInfo = (token: string) => {
 
@@ -78,16 +89,31 @@ const Cart: React.FC = () => {
                 return response.json();  // 解析 JSON 回應
             })
             .then(data => {
-                setcatchProduct(Product);
+                const updatedData = data.map((product: any, index: number) => ({
+                    ...product,
+                    key: index,  // 使用從0開始的索引作為key
+                }));
+                setcatchProduct(updatedData);
                 sethasProduct(true);
-                setcatchProduct(data); // 在這裡處理 API 返回的數據
             })
             .catch(error => {
                 seterror(true);  // 處理錯誤
             });
     };
-    if (token == null) {
-        return (
+    /* Checkbox Modify*/
+    const [selectkey,setselectkey] = useState<React.key[]>([]);
+    const SelectChange =  (newSelectedRowKeys: React.Key[]) => {
+        setselectkey(newSelectedRowKeys);
+    }
+    const rowSelection: TableRowSelection<ProductInCart> = {
+        selectkey,
+        onChange: SelectChange,
+    };
+    useEffect(() => {
+        fetchUserInfo(token);
+    }, []);
+    return (
+        token == null ? (
             <div className="container">
                 <Flex>
                     <Layout style={layoutStyle}>
@@ -97,36 +123,40 @@ const Cart: React.FC = () => {
                     </Layout>
                 </Flex>
             </div>
-        )
-    }
-    fetchUserInfo(token);
-
-
-    if (hasProduct) {
-        return (
-            <div className="container">
-                <h1 style={{ fontSize: '100px' }}>Cart Information</h1>
-                <Table <ProductInCart>
-                    rowSelection={{ type: productselect, ...rowSelection }}
-                    dataSource={Product}
-                    columns={CartTable}
-                >
-                </Table>
-                <br></br>
-                <div style={{ marginLeft: '100px' }}>
-                    <button style={{ transform: 'scale(1.5)' }}>購買</button>
-                    <button style={{ marginLeft: '100px', transform: 'scale(1.5)' }}>刪除</button>
+        ) : (
+            hasProduct?(
+                <div className="container">
+                    <h1 style={{fontSize: '100px'}}>Cart Information</h1>
+                    <Table<ProductInCart>
+                        rowSelection={{SelectChange}}
+                        dataSource={Product}
+                        columns={CartTable}
+                    >
+                    </Table>
+                    <br></br>
+                    <div style={{marginLeft: '100px'}}>
+                        <button style={{transform: 'scale(1.5)'}}>購買</button>
+                        <button style={{marginLeft: '100px', transform: 'scale(1.5)'}}>刪除</button>
+                    </div>
                 </div>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div>
-                <h1 style={{ fontSize: '90px', textAlign: 'center', marginTop: '90px' }}> 你他媽的應該要先買東西再來點那該死的購物車 </h1>
-            </div>
+            ): (
+                <div className="container">
+                    <h1 style={{
+                        fontSize: '90px',
+                        textAlign: 'center',
+                        marginTop: '90px',
+                        whiteSpace: 'normal'
+                    }}> 你他媽的應該要先買東西 </h1>
+                    <h1 style={{
+                        fontSize: '90px',
+                        textAlign: 'center',
+                        marginTop: '90px',
+                        whiteSpace: 'normal'
+                    }}> 再來點那該死的購物車 </h1>
+                </div>
+            )
         )
-    }
+    );
 
 }
 
