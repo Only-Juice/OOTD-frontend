39c045d2a96fde994d3329695e48ea87cb12f37b
ZRE <chy853@gmail.com>
2024-12-27 23:18:24 +0800

    Add error alert for unauthenticated users on Admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { Card, Button } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Alert, Card, Button, Skeleton } from 'antd';
 import AddCouponForm from '../components/AddCouponForm';
 import GiveCoupon from '../components/GiveCoupon';
 import ModifyCoupon from '../components/ModifyCoupon';
 
 const Admin: React.FC = () => {
+    const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
     const [selectedComponent, setSelectedComponent] = useState('add');
 
     const renderComponent = () => {
@@ -20,16 +21,24 @@ const Admin: React.FC = () => {
         }
     };
 
+    useEffect(() => {
+        setToken(localStorage.getItem('token'));
+    }, [localStorage.getItem('token')]);
+
     return (
         <Card>
-            <div className='mb-2' style={{ display: 'flex', gap: '10px' }}>
+            {token ? <><div className='mb-2' style={{ display: 'flex', gap: '10px' }}>
                 <Button onClick={() => setSelectedComponent('add')}>新增優惠券</Button>
                 <Button onClick={() => setSelectedComponent('give')}>給優惠券</Button>
                 <Button onClick={() => setSelectedComponent('modify')}>修改優惠券</Button>
             </div>
-            <hr />
-            {renderComponent()}
-        </Card>
+                <hr />
+                {renderComponent()}</> :
+                <Alert className='mb-2'
+                    message="錯誤"
+                    description="請先登入帳號"
+                    type="error" showIcon />}
+        </Card >
     );
 };
 
