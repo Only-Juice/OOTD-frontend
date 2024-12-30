0265eddbba18824440bc7045410b430dd978301d
ZRE <chy853@gmail.com>
2024-12-31 04:00:19 +0800

    Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 const Home = React.lazy(() => import('./pages/Home'));
 const Cart = React.lazy(() => import('./pages/Cart.tsx'));
